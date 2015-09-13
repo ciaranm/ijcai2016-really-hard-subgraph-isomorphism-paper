9b99705c39263b1a514c8453e8281cc61788a28f
Ciaran McCreesh <ciaran.mccreesh@googlemail.com>
2015-09-13 18:32:08 +0100

    Import Glucose

---
+++ sat/glucose-syrup/simp/depend.mk
@@ -0,0 +1,40 @@
+/home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/SimpSolver.o /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/SimpSolver.or /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/SimpSolver.od /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/SimpSolver.op: \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/SimpSolver.cc \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../mtl/Sort.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../mtl/Vec.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../mtl/IntTypes.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../mtl/XAlloc.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../simp/SimpSolver.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../mtl/Queue.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../core/Solver.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../mtl/Heap.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../mtl/Alg.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../utils/Options.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../utils/ParseUtils.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../core/SolverTypes.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../mtl/Map.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../mtl/Alloc.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../core/BoundedQueue.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../core/Constants.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../mtl/Clone.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../utils/System.h
+/home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/Main.o /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/Main.or /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/Main.od /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/Main.op: \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/Main.cc \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../utils/System.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../mtl/IntTypes.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../utils/ParseUtils.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../utils/Options.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../mtl/Vec.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../mtl/XAlloc.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../core/Dimacs.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../core/SolverTypes.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../mtl/Alg.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../mtl/Map.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../mtl/Alloc.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../simp/SimpSolver.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../mtl/Queue.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../core/Solver.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../mtl/Heap.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../core/BoundedQueue.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../core/Constants.h \
+ /home/users/ciaranm/really-hard-subgraph-isomorphism-paper/sat/glucose-syrup/simp/../mtl/Clone.h
